07b7fe3b1e0fece9765a1f16f682dca1757a4fba
James Campbell <james@supmenow.com>
2015-12-05 19:04:58 +0000

    SMS Message.

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -17,8 +17,28 @@ class ReceiveTextController < ActionController::Base
       'api_token' => 'xoxb-16016274358-R8JWOSfV9BNOfZPhLu1GfUeL'
     }
 
-    bot = Slackbotsy::Bot.new(config) do
-    end
+    bot = Slackbotsy::Bot.new(config)
     bot.post_message("#{message_body}")
+  end
+
+  def hear
+    bot = Slackbotsy::Bot.new(config) do
+      hear /echo\s+(.+)/ do |mdata|
+
+        number_to_send_to = params[:number_to_send_to]
+
+        twilio_sid = "AC4323dda0811444bd2f359135fa41e002"
+        twilio_token = "8fab2a62bdc97c913c27f1d7c42e30bb"
+        twilio_phone_number = "+441706304028"
+
+        @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+        @twilio_client.account.sms.messages.create(
+          :from => "+1#{twilio_phone_number}",
+          :to => "+447523279698",
+          :body => "I heard #{user_name} say '#{mdata[1]}' in #{channel_name}"
+        )
+      end
+    end
   end
 end
