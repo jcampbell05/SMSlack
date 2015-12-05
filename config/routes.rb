762c4c64d08d1b60c1ec1205bfbec782366e394f
James Campbell <james@supmenow.com>
2015-12-05 19:29:38 +0000

    Switch to post.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'receive_text#index'
-  get  'hear' => 'receive_text#hear'
+  post  'hear' => 'receive_text#hear'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
